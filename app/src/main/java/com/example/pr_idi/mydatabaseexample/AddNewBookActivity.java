7498dc4eca835541d5eef02c9f23e9e55b3af0a8
martihomssoler <martihomssoler@gmail.com>
2017-01-09 19:11:04 +0100

    tercer commit d'en Holmez

---
+++ app/src/main/java/com/example/pr_idi/mydatabaseexample/AddNewBookActivity.java
@@ -0,0 +1,13 @@
+package com.example.pr_idi.mydatabaseexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddNewBookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_book);
+    }
+}
